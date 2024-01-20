b11ef6f30fbf1c983cc079120edcedbea4def674
Tarandeep Singh <tarandeep.singh562@gmail.com>
2024-01-19 17:32:22 -0800

    Update module-info.java

--- ModulDemo2BankImpl/src/modul.demo.bank.impl/module-info.java
+++ ModulDemo2BankImpl/src/modul.demo.bank.impl/module-info.java
@@ -1,6 +1,5 @@
 module modul.demo.bank.impl {
 	requires modul.demo.bank;
-	exports modul.demo.bank.impl;
 	provides modul.demo.bank.IAccountProvider 
 			with modul.demo.bank.impl.AccountProvider;
 }
